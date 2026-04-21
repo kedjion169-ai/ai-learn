229eeb6dc2d58e8cd088da1666cd68b31693dd26
ked <kedjion169@gmail.com>
2026-04-21 15:15:56 +0800

    exe11111

---
+++ d-ai.py
@@ -0,0 +1,16 @@
+import json
+
+# Day 12：用 AI 助手写代码练习
+
+
+
+def generate_ai_response():
+    # 模拟AI返回的内容
+    ai_response = {
+        "role": "温柔御姐",
+        "voice": "女",
+        "text": "早安呀，今天也要加油哦\~",
+        "speed": 1.0,
+        "emotion": "happy"
+    }
+    return ai_response
